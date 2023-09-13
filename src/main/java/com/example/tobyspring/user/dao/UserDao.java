dcfc2c069015aa5cd0c4e627f67d83e8250d3620
hanchaeri <coflzz@naver.com>
2023-09-14 00:09:32 +0900

    tobySpring 3장 템플릿콜백

--- src/main/java/com/example/tobyspring/user/dao/UserDao.java
+++ src/main/java/com/example/tobyspring/user/dao/UserDao.java
@@ -6,21 +6,8 @@ import javax.sql.DataSource;
 import java.sql.*;
 
 public class UserDao {
-    //private ConnectionMaker connectionMaker;
     private DataSource dataSource;
     private JdbcContext jdbcContext;
-
-    /*public UserDao(ConnectionMaker connectionMaker) {
-        this.connectionMaker = connectionMaker;
-    }*/
-
-    /*public void setConnectionMaker(ConnectionMaker connectionMaker) {
-        this.connectionMaker = connectionMaker;
-    }*/
-
-    /*public void setJdbcContext(JdbcContext jdbcContext) {
-        this.jdbcContext = jdbcContext;
-    }*/
 
     public void setDataSource(DataSource dataSource) {
         this.jdbcContext = new JdbcContext();
@@ -42,44 +29,6 @@ public class UserDao {
                 }
             }
         );
-        //Connection c = connectionMaker.makeConnection();
-        /*Connection c = dataSource.getConnection();
-
-        PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
-        ps.setString(1, user.getId());
-        ps.setString(2, user.getName());
-        ps.setString(3, user.getPassword());
-
-        ps.executeUpdate();
-
-        ps.close();
-        c.close();*/
-
-        /*class AddStatement implements StatementStrategy {
-            @Override
-            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-
-                PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
-                ps.setString(1, user.getId());
-                ps.setString(2, user.getName());
-                ps.setString(3, user.getPassword());
-
-                return ps;
-            }
-        }
-
-        jdbcContextWithStatementStrategy (
-            new AddStatement() {
-                public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                    PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
-                    ps.setString(1, user.getId());
-                    ps.setString(2, user.getName());
-                    ps.setString(3, user.getPassword());
-
-                    return ps;
-                }
-            }
-        );*/
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
@@ -104,50 +53,7 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        this.jdbcContext.workWithStatementStrategy(
-            new StatementStrategy() {
-                public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                    PreparedStatement ps = c.prepareStatement("delete from users");
-                    return ps;
-                }
-            }
-        );
-        //Connection c = connectionMaker.makeConnection();
-        /*Connection c = null;
-        PreparedStatement ps = null;
-
-        try {
-            c =dataSource.getConnection();
-            //ps = makeStatement(c);//c.prepareStatement("delete from users");
-            StatementStrategy strategy = new DeleteAllStatement();
-            ps = strategy.makePreparedStatement(c);
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if(ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                }
-            }
-            if(c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {
-                }
-            }
-        }*/
-
-        /*StatementStrategy st = new DeleteAllStatement();
-        jdbcContextWithStatementStrategy(
-            new DeleteAllStatement() {
-                public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                    PreparedStatement ps = c.prepareStatement("delete from users");
-                    return ps;
-                }
-            }
-        );*/
+        this.jdbcContext.executeSql("delete from users");
     }
 
     public int getCount() throws SQLException {
@@ -186,10 +92,6 @@ public class UserDao {
         }
         return 0;
     }
-    //public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
-
-    //private PreparedStatement makeStatement(Connection c) throws SQLException {
-    //abstract protected PreparedStatement makeStatement(Connection c) throws SQLException;
 
     public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
         Connection c = null;
---
+++ src/test/java/com/example/tobyspring/learningtest/template/CalcSumTest.java
@@ -0,0 +1,24 @@
+package com.example.tobyspring.learningtest.template;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class CalcSumTest {
+    @Test
+    public void sumOfNumbers() throws IOException {
+        Calculator calculator = new Calculator();
+        assertThat(calculator.calcSum(getClass().getResource("/txt/numbers.txt").getPath())).isEqualTo(10);
+    }
+
+    @Test
+    public void multiplyOfNumbers() throws IOException {
+        Calculator calculator = new Calculator();
+        assertThat(calculator.calcMultiply(getClass().getResource("/txt/numbers.txt").getPath())).isEqualTo(24);
+    }
+}
